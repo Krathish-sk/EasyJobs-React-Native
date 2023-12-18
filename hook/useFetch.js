102d79d62bd530f62e400ce8a7cb12c621cbe011
Krathish K <73063741+Krathish-sk@users.noreply.github.com>
2023-12-18 16:15:18 +0530

    Update useFetch.js (api key)

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -11,7 +11,7 @@ export default function useFetch(endpoint, query) {
     url: `https://jsearch.p.rapidapi.com/${endpoint}`,
     params: { ...query },
     headers: {
-      "X-RapidAPI-Key": "71d5b33e54msh230d124660b0d40p1f5c9cjsn1976dda2d1ec",
+      "X-RapidAPI-Key": "",
       "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
     },
   };
